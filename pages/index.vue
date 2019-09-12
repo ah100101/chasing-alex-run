e601660c0bc17bad63a8fd764b2d348cd86ed827
ah100101 <ah100101@gmail.com>
2019-09-11 23:32:52 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
