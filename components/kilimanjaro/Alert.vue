18b859d97bae0febbad6c96a659231373e946e53
ah100101 <ah100101@gmail.com>
2020-01-28 08:45:01 -0600

    worked on countdown timer

--- components/kilimanjaro/Alert.vue
+++ components/kilimanjaro/Alert.vue
@@ -14,17 +14,17 @@ export default {
   },
   mounted: function () {
     if (process.client) this.tellEveryoneAboutKiliTracker()
+    // this.setTimer()
   },
   data: function () {
     return {
-      
+      tzTime: moment().tz("Africa/Nairobi")
     }
   },
   methods: {
     tellEveryoneAboutKiliTracker() {
       this.$buefy.snackbar.open({
-        // type: 'is-accent',
-        message: 'The Kilimanjaro climb begins in ',
+        message: 'The Kilimanjaro climb begins in ' + this.countDown,
         position: 'is-bottom-left',
         actionText: 'Follow Along',
         indefinite: true,
@@ -32,10 +32,39 @@ export default {
             
         }
       })
-    }
+    },
+    // setTimer () {
+    //   setInterval(() => { 
+    //     this.tzTime = moment().tz('Africa/Nairobi')
+    //   }, 1000)
+    // },
   },
   computed: {
+    countDown() {
+      let duration = moment.duration(moment.tz(mapData.movement[0].start, 'Africa/Nairobi').diff(this.tzTime)) 
+      let days = duration.days()
+      let hours = duration.hours()
+      let minutes = duration.minutes()
+      let seconds = duration.seconds()
 
+      if (days > 1 && hours > 1) {
+        return `${days} days and ${hours} hours`
+      } else if (days > 1 && hours === 1) {
+        return `${days} days and 1 hours`
+      } else if (days === 1 && hours > 1) {
+        return `${days} day and ${hours} hours`
+      } else if (days === 1 && hours === 1) {
+        return `${days} day and ${hours} hour`
+      } else if (days < 1 && hours > 1) {
+        return `${hours} hours`
+      } else if (days < 1 && hours === 1) {
+        return `1 hour`
+      } else if (days < 1 && hours < 1 && minutes > 0) {
+        return `soon`
+      }
+
+      return false
+    }
   }
 }
 </script>
